b45b9273cb99c2142b1d2ee4624e5a3be6d87efd
bhargav-yarlagadda <bhargavyarlagadda2003@gmail.com>
2024-11-02 19:33:33 +0530

    chore

--- src/components/FeaturedSection.tsx
+++ src/components/FeaturedSection.tsx
@@ -1,5 +1,6 @@
 import courseData from '../data/music_courses.json'
 import { Card } from './Card'
+import { InfiniteMovingCard } from './MovingCards'
 import { StickyScrollSection } from './StickyScroll'
 
 const FeaturedSection = () => {
@@ -12,14 +13,15 @@ const FeaturedSection = () => {
         </div>
         <div className='flex flex-wrap justify-center gap-5'>
             {
-                courseData.courses.map((courser)=>{
+                courseData.courses.map((courser,index)=>{
                 return(
-                    <Card title={courser.title} description={courser.description} instructor={courser.instructor} image={courser.image} price={courser.price} />
+                    <Card title={courser.title} key={index} description={courser.description} instructor={courser.instructor} image={courser.image} price={courser.price} />
                 )
                 })
             }
         </div>
         <StickyScrollSection/>
+        <InfiniteMovingCard/>
     </div>
   )
 }
